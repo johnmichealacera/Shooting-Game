225b721c30a3a57bf5d29378b8fc363f8b081b57
John Acera <john.acera@sportality.se>
2023-04-07 14:59:27 +0800

    update site title to bird hunter, added levels and increase bird speed per level and code enhancements

--- index.js
+++ index.js
@@ -1,77 +1,82 @@
 let main = document.getElementById('main');
 let score = document.getElementById('score');
+let levelLabel = document.getElementById('level');
 let boom = document.getElementById("myAudio");
 let game = document.getElementById('gameover'); 
 let background = document.getElementById('background'); 
 let start = document.getElementById('start');
 let count;
-let birdInterval;
+let level = 1;
+let flightSpeed = 50;
+let spawnTime = 3000;
+let gameState;
 
-function showBird(){
-	if(main.childElementCount<20){
+const showBird = () => {
+	if (gameState === 'playing') {
 		let bird = document.createElement('img');
-		let position = 1500;
+		let position = 1400;
 		bird.src = 'bird.webp';
 		bird.style.height = '80px';
 		bird.style.width = '80px';
 		bird.style.position = 'absolute';
-    const screenSize = document.documentElement.clientWidth || window.innerWidth;
-      if (screenSize <= 480) {
-        bird.style.top = Math.random()*300+'px';
-      } else {
-        bird.style.top = Math.random()*650+'px';
-      }
+		const screenSize = document.documentElement.clientWidth || window.innerWidth;
+		if (screenSize <= 480) {
+			bird.style.top = Math.random()*300+'px';
+		} else {
+			bird.style.top = Math.random()*650+'px';
+		}
 		bird.style.right = position+'px';
 		main.appendChild(bird);
-		
-		let birdFlight = setInterval(flight, 10);
-		function flight(){
-				if(main?.children[0]?.style?.right === '0px'){
-					clearInterval(birdFlight);
-					clearInterval(birdInterval);
-					main.innerHTML = "";
-					background.pause();
-					start.style.display = 'block';
-					alert('You did not shoot all the birds. Game over.');
-				}
-				else{
-					bird.style.right = position+'px';
-					position--;
-				}
+		const flight = () => {
+			if(Array.from(main?.children)?.find((child) => child.style.right === '0px')){
+				main.innerHTML = "";
+				background.pause();
+				start.style.display = 'block';
+				game.play();
+				gameState = 'gameOver';
+				alert('You did not shoot all the birds. Game over.');
+				clearInterval(birdFlight);
+				clearInterval(birdInterval);
 			}
-	}
-	else{
-		clearInterval(birdInterval);
-	}
+			else{
+				bird.style.right = position+'px';
+				position--;
+			}
+		}
+		const birdFlight = () => setInterval(flight, flightSpeed);
+		birdFlight();
+	}	
 }
-start.addEventListener('click', function(){
+
+const birdInterval = () => setInterval(showBird, spawnTime);
+
+start.addEventListener('click', () => {
+	gameState = 'playing';
+	flightSpeed = 50;
+	spawnTime = 6000;
+	level = 1;
+	levelLabel.innerHTML = `Level ${level}`;
 	start.style.display = 'none';
 	count = 0;
+	score.innerHTML = `Score is ${count}`;
 	background.play();
 	background.loop = true;
-	birdInterval = setInterval(showBird, 2000);
-	
+	birdInterval();
 });
 	
-main.addEventListener('click', function(ev){
-	if(ev.target.nodeName === 'IMG'){
+main.addEventListener('click', (ev) => {
+	if (ev.target.nodeName === 'IMG') {
 		boom.play();
 		ev.target.remove();
 		count++;
 		score.innerHTML = `Score is ${count}`;
-		if(count === 20){
-			main.innerHTML = "";
-			score.innerHTML = '';
-			background.pause();
-			game.play();
-			const willPlayAgain = confirm('Congrats on clearing the game, play again?');
-			if (!willPlayAgain) {
-				start.style.display = 'block';
-				background.pause();
-				clearInterval(birdInterval);
-			} else {
-				start.click();
-			}
+		if (count % 10 === 0 && gameState === 'playing') {
+			spawnTime = spawnTime - level*100;
+			level++;
+			levelLabel.innerHTML = `Level ${level}`;
+			flightSpeed = flightSpeed-count;
+			clearInterval(birdInterval);
+			birdInterval();
 		}
 	}
 });
@@ -81,3 +86,11 @@ main.addEventListener('mouseover', function(ev){
 		ev.target.style.cursor = 'crosshair';
 	}
 });
+
+const loadResources = () => {
+	background.preload = true;
+	boom.preload = true;
+	game.preload = true;
+}
+
+loadResources();
